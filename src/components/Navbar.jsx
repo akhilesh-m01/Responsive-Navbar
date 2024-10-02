4e848e782c29f71bda186de465112af46fc2859e
Akhilesh M <58871782+akhilesh-m01@users.noreply.github.com>
2024-10-02 19:43:40 +0000

    responsive nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Navbar = () =>{
+
+    const [open,setOpen] = useState(false);
+
+    const toggleHandler = () =>{
+        setOpen(!open)
+    }
+
+    return(
+        <div className='mx-auto block md:flex justify-between items-center bg-slate-500 p-4'>
+            <div className=''>
+                <p>Logo</p>
+            </div>
+            {/* medium screen */}
+            <div className="hidden md:flex gap-2 list-none">
+                <li>About</li>
+                <li>Projects</li>
+                <li>Experience</li>
+                <li>Contact</li>
+            </div>
+
+            {/* small screens */}
+            <div className="flex flex-col md:hidden list-none">
+                <button onClick={toggleHandler} className='flex'>Menu</button>
+                <div className={`${open?'hidden':'block'} bg-slate-500 text-white`}>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Experience</li>
+                    <li>Contact</li>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
